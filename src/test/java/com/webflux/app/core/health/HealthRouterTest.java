fa7ea6dcce78f1ca14c4418410e7bb5bfbc8a193
Wagner <wagner.carvalho@mercadolivre.com>
2019-07-23 13:22:51 -0300

    Codecov (#1)
    * include codecov
    
    * test
    
    * Wip

--- src/test/java/com/webflux/app/core/health/HealthRouterTest.java
+++ src/test/java/com/webflux/app/core/health/HealthRouterTest.java
@@ -1,7 +1,7 @@
 package com.webflux.app.core.health;
 
-
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
