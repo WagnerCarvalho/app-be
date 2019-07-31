f0131b169e0e3973a0168599dfc53de0f2df0791
Wagner Carvalho <wagner.carvalho@mercadolivre.com>
2019-07-31 08:26:41 -0300

    add readme

--- src/main/java/com/webflux/app/AppApplication.java
+++ src/main/java/com/webflux/app/AppApplication.java
@@ -2,12 +2,8 @@ package com.webflux.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("com.webflux.app.business.domain")
-@EnableJpaRepositories("com.webflux.app.business.repository")
 public class AppApplication {
 
   public static void main(final String[] args) {
--- src/main/java/com/webflux/app/core/health/HealthRouter.java
+++ src/main/java/com/webflux/app/core/health/HealthRouter.java
@@ -1,6 +1,7 @@
 package com.webflux.app.core.health;
 
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
